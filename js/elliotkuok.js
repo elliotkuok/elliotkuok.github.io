e9c8a2b029c299401054fe15b6765b8d318bffd6
Elliot Kuok <elliotckuok@gmail.com>
2018-07-23 17:02:07 -0400

    Made loading stats bars more efficient

--- js/elliotkuok.js
+++ js/elliotkuok.js
@@ -19,120 +19,24 @@ $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-//Activates skill bars when scrolled on screen
-//window.sr = ScrollReveal({ reset: true });
-//sr.reveal('.skill-percent',{
-//  origin: 'left',
-//  delay: 100,
-//  opacity: 0,
-//});
-
-
-$(document).ready(function() {
-  $('#ms-word').waypoint(function() {
-    $('#ms-word').css({
-    animation: "loading 1s linear",
-    opacity: "1"
+function myAnimation(selector){
+selector.waypoint(function() {
+    selector.css({
+      animation: "loading 1s linear",
+      opacity: "1"
     });
-    }, { offset: '95%' });
-});
+  }, { offset: '95%' });
+}
 
-$(document).ready(function() {
-  $('#powerpoint').waypoint(function() {
-    $('#powerpoint').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#excel').waypoint(function() {
-    $('#excel').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#hubspot').waypoint(function() {
-    $('#hubspot').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#raiser').waypoint(function() {
-    $('#raiser').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#wordpress').waypoint(function() {
-    $('#wordpress').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#photoshop').waypoint(function() {
-    $('#photoshop').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#illustrator').waypoint(function() {
-    $('#illustrator').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#indesign').waypoint(function() {
-    $('#indesign').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#htmlcss').waypoint(function() {
-    $('#htmlcss').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#javascript').waypoint(function() {
-    $('#javascript').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
-$(document).ready(function() {
-  $('#python').waypoint(function() {
-    $('#python').css({
-    animation: "loading 1s linear",
-    opacity: "1"
-    });
-    }, { offset: '95%' });
-});
-
+myAnimation($('#ms-word'));
+myAnimation($('#powerpoint'));
+myAnimation($('#excel'));
+myAnimation($('#hubspot'));
+myAnimation($('#raiser'));
+myAnimation($('#wordpress'));
+myAnimation($('#photoshop'));
+myAnimation($('#illustrator'));
+myAnimation($('#indesign'));
+myAnimation($('#htmlcss'));
+myAnimation($('#javascript'));
+myAnimation($('#python'));
